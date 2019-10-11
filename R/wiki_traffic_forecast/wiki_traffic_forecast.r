305b5e193e3968745ce9c2044e9007710952d0a7
Ajay Kumar <ajay.kumar@galytix.com>
2019-10-11 19:54:04 +0530

    changes in wiki time series code

--- R/wiki_traffic_forecast/wiki_traffic_forecast.r
+++ R/wiki_traffic_forecast/wiki_traffic_forecast.r
@@ -98,12 +98,12 @@ tpages <-  wikipedia %>% full_join(wikimedia, by=c("rowname","article","locale",
 
 extract_ts <- function(rownr){
   dataTime <- tdates[rownr,]
-  outDf <- data.frame(dates=colnames(dataTime),views=as.numeric(df[1,]))
+  outDf <- data.frame(dates=colnames(dataTime),views=as.numeric(dataTime[1,]))
   return(outDf)
 }
 extract_ts_nrm <- function(rownr){
   dataTime <- tdates[rownr,]
-  outDf <- data.frame(dates=colnames(dataTime),views=as.numeric(df[1,]))
+  outDf <- data.frame(dates=colnames(dataTime),views=as.numeric(dataTime[1,]))
   outDf <- outDf %>% mutate(views=views/mean(views))
   return(outDf)
 }
@@ -111,7 +111,7 @@ plot_rownr <- function(rownr){
   art <- tpages[rownr,]$article
   loc <- tpages[rownr,]$locale
   acc <- tpages[rownr,]$access
-  extract_ts(rownr) %>% ggplot(aes(x=dates,y=views))+geom_line()+
+  extract_ts(rownr) %>% ggplot(aes(x=dates,y=views,group = 1))+geom_line()+
     geom_smooth(method = "loess", color="blue", span=1/5)+
     labs(title = paste(art,"-",loc,"-",acc))
 }
@@ -119,7 +119,7 @@ plot_rownr_log <- function(rownr){
   art <- tpages[rownr,]$article
   loc <- tpages[rownr,]$locale
   acc <- tpages[rownr,]$access
-  extract_ts_nrm(rownr) %>% ggplot(aes(x=dates, y=views))+ geom_line()+
+  extract_ts_nrm(rownr) %>% ggplot(aes(x=dates, y=views,group = 1))+ geom_line()+
     geom_smooth(method = "loess", color="red", span=1/5)+
     labs(title = paste(art,"-",loc,"-",acc))+
     scale_y_log10()+labs(y="log views")
@@ -131,7 +131,7 @@ plot_rownr_zoom <- function(rownr,start,end){
   acc <- tpages[rownr,]$access
   data1 <- extract_ts(rownr)
   data1 <- data1[data1$dates > ymd(start) & data1$dates <= ymd(end),]
-  data1 %>% ggplot(aes(x=dates, y=views))+ geom_line()+
+  data1 %>% ggplot(aes(x=dates, y=views,group = 1))+ geom_line()+
     geom_smooth(method = "loess", color="red", span=1/5)+
     labs(title = paste(art,"-",loc,"-",acc))
   
@@ -158,26 +158,77 @@ plot_names_nrm <-  function(art, acc, agent){
   df %>% ggplot(aes(x=variable,y=value,color=locale))+geom_line()+scale_y_log10()
 }
 
+plot_rownr(11214)
+plot_names("The_Beatles", "all-access", "all-agents")
+p1 <- tpages %>% ggplot(aes(agent))+geom_bar(fill="red")
+p2 <- tpages %>% ggplot(aes(access))+geom_bar(fill="red")
+p3 <- tpages %>% ggplot(aes(locale, fill=locale))+geom_bar()+theme(legend.position = "none")
+layout <- matrix(c(1,2,3,3),2,2, byrow = T)
+multiplot(p1,p2,p3,layout = layout)
+
+params_ts1 <- function(rownr){
+df <- extract_ts(rownr)
+df2 <- as.data.frame(matrix(data = NA, ncol = 6,nrow = 1))
+colnames(df2) <- c("min_view","max_view","mean_view","med_view","sd_view","sd_by_mean_view")
+df2$min_view  <- min(df$views)
+df2$max_view <- max(df$views)
+df2$mean_view <- mean(df$views)
+df2$med_view <- median(df$views)
+df2$sd_view <- sd(df$views)
+df2$sd_by_mean_view <- sd(df$views)/mean(df$views)
+return(df2)
+}
+params_by_locale <-  function(rowUpto=10000){
+dftemp <- tdates %>% rownames_to_column(var = "rowname")
+dftemp <- merge(tpages,dftemp, by="rowname")
+  dfStore <- as.data.frame(matrix(data = NA, ncol = 10,nrow = nrow(tdates)))
+  colnames(dfStore) <- append(colnames(tpages)[!colnames(tpages) %in% "rowname"],
+                              c("min_view", "max_view", "mean_view", "med_view",  "sd_view","sd_by_mean_view"))
+  for (i in 1: rowUpto) {
+    print(i)
+    dftemp2 <- params_ts1(rownr = i)
+    dfStore[i,"article"] <- dftemp$article[i]
+    dfStore[i,"locale"] <- dftemp$locale[i]
+    dfStore[i,"access"] <- dftemp$access[i]
+    dfStore[i,"agent"] <- dftemp$agent[i]
+    dfStore[i,"min_view"] <- dftemp2$min_view
+    dfStore[i,"max_view"] <- dftemp2$max_view
+    dfStore[i,"mean_view"] <- dftemp2$mean_view
+    dfStore[i,"med_view"] <- dftemp2$med_view
+    dfStore[i,"sd_view"] <- dftemp2$sd_view
+    dfStore[i,"sd_by_mean_view"] <- dftemp2$sd_by_mean_view
+  }
+  return(dfStore)
+}
+
+# paramsLocalDf <- params_by_locale()
+p1 <- paramsLocalDf %>% ggplot(aes(min_view))+
+  geom_histogram(fill="red", bins = 50)+scale_x_log10()
+p2 <- paramsLocalDf %>% ggplot(aes(max_view))+
+  geom_histogram(fill="red", bins = 50)+scale_x_log10()
+p3 <- paramsLocalDf %>% ggplot(aes(mean_view))+
+  geom_histogram(fill="red", bins = 50)+scale_x_log10()
+p4 <- paramsLocalDf %>% ggplot(aes(med_view))+
+  geom_histogram(fill="red", bins = 50)+scale_x_log10()
+p5 <- paramsLocalDf %>% ggplot(aes(sd_view))+
+  geom_histogram(fill="red", bins = 50)+scale_x_log10()
+p6 <- paramsLocalDf %>% ggplot(aes(sd_by_mean_view))+
+  geom_histogram(fill="red", bins = 50)+scale_x_log10()
+layout <- matrix(1:6,3,2,byrow = T)
+multiplot(p1,p2,p3,p4,p5,p6, layout=layout)
 
 
 
-# p1 <- tpages %>% ggplot(aes(agent))+geom_bar(fill="red")
-# p2 <- tpages %>% ggplot(aes(access))+geom_bar(fill="red")
-# p3 <- tpages %>% ggplot(aes(locale, fill=locale))+geom_bar()+theme(legend.position = "none")
-# layout <- matrix(c(1,2,3,3),2,2, byrow = T)
-# multiplot(p1,p2,p3,layout = layout)
-# extract_ts(rownr = 10)
-
-
-
-
-
-
-
-
-
-
-
+rownr <- 139120
+pageviews <- extract_ts(rownr) %>%rename(y = views,ds = dates)
+pred_len <- 60
+pred_range <- c(nrow(pageviews)-pred_len+1, nrow(pageviews))
+pre_views <- pageviews %>% head(nrow(pageviews)-pred_len)
+post_views <- pageviews %>% tail(pred_len)
+proph <- prophet(pre_views, changepoint.prior.scale=0.5, yearly.seasonality=TRUE)
+future <- make_future_dataframe(proph, periods = pred_len)
+fcast <- predict(proph, future)
+plot(proph, fcast)
 
 
 
