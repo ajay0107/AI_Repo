848ba06a9b723104a536700400fe077de80361ce
Ajay Kumar <ajay01071992@gmail.com>
2019-10-20 02:54:41 +0530

    unit jupyter file

--- Python/project_1/Deep_learning/image_data_augmentation_tutorial.py
+++ Python/project_1/Deep_learning/image_data_augmentation_tutorial.py
@@ -5,37 +5,28 @@ from PIL import Image
 from IPython.display import SVG
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from keras.preprocessing import image as imgk
+from keras.preprocessing import image
 
+
+# loading image
 inputPath = "Deep_learning/data/bird.jpg"
 
-# =============================================================================
-# We will focus on five main types of data augmentation 
-# techniques for image data; specifically:
-# =============================================================================
 
-# =============================================================================
-# Image shifts via the width_shift_range and height_shift_range arguments.
-# Image flips via the horizontal_flip and vertical_flip arguments.
-# Image rotations via the rotation_range argument
-# Image brightness via the brightness_range argument.
-# Image zoom via the zoom_range argument.
-# =============================================================================
 
-# =============================================================================
-# For example, an instance of the ImageDataGenerator class can be constructed.
-# =============================================================================
 
-# create data generator
-datagen = ImageDataGenerator()
 
-# =============================================================================
-# An iterator can be created from an image dataset loaded in memory 
-# via the flow() function; for example:
-# =============================================================================
 
-# load the image
-img = imgk.load_img(inputPath)
-# convert image to numpy array 
-data = imgk.img_to_array(img)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
